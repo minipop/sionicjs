c804e22f5aabd2a89b7b8a5461d15066bfb2c529
fct_2004@mail.goo.ne.jp <fct_2004@mail.goo.ne.jp>
2021-05-22 21:47:52 +0900

    fix preproces by kt

--- dist/sionic.js
+++ dist/sionic.js
@@ -659,7 +659,7 @@ module.exports = [
 },{}],4:[function(require,module,exports){
 module.exports = function(mml){
     //最初に見つかったテンポ表記を全トラックに適用する
-    var found = mml.match(/t([0-9]+)/);
+    var found = mml.match(/^kt([0-9]+)/);
     
     if(found && found[1]){
         mml = ""+mml.split(";").map(function(track){
